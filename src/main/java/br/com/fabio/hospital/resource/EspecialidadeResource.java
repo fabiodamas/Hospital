71be753ff6626bd2c987f4e97dfa8272eb197309
Fabio Damas <fabio.damas@gmail.com>
2021-05-06 07:27:55 -0300

    Creating the exception handler

--- src/main/java/br/com/fabio/hospital/resource/EspecialidadeResource.java
+++ src/main/java/br/com/fabio/hospital/resource/EspecialidadeResource.java
@@ -3,20 +3,36 @@
 import br.com.fabio.hospital.model.Especialidade;
 import br.com.fabio.hospital.repository.EspecialidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/especialidades")
 public class EspecialidadeResource {
-    @Autowired
-     private EspecialidadeRepository especialidadeRepository;
+    private EspecialidadeRepository especialidadeRepository;
+
+    public EspecialidadeResource(EspecialidadeRepository especialidadeRepository) {
+        this.especialidadeRepository = especialidadeRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Especialidade> criar(@RequestBody Especialidade especialidade,  HttpServletResponse response){
+        Especialidade especialidadeSalva = especialidadeRepository.save(especialidade);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(especialidadeSalva.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(especialidadeSalva);
+    }
 
     @GetMapping
-    public List<Especialidade> listar(){
+    public  List<Especialidade> listar(){
         return especialidadeRepository.findAll();
     }
 }
